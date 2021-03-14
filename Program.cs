8b91bc8279f7e527734141f1b90afc6931f47527
Airezigma <gabs.sfv@gmail.com>
2021-03-14 14:44:03 -0300

    cebolinha

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+
+            //<---Inicio(Digite)--->//
+            Console.Write("Digite Qualquer Coisa: ");
+            frase = Console.ReadLine();
+            Console.Write("\n");
+
+            Console.WriteLine($"{frase.Replace("r", "l").Replace("R", "L")}");
+            //<---Fim(Digite)--->//
+
+            //<---Inicio(Sair)--->//
+            Console.WriteLine("Pressione ENTER para sair");
+            
+            //<---Fim(Sair)--->//
+            
+            Console.ReadKey();
+        }
+    }
+}
